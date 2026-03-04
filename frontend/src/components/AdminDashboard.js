fc48a719f681d508cf599066e0aacf1231c16189
2k22eee38-boop <2k22eee38@kiot.ac.in>
2026-03-04 21:38:01 +0530

    Modified the files version 1.0.1

--- frontend/src/components/AdminDashboard.js
+++ frontend/src/components/AdminDashboard.js
@@ -1,36 +1,3 @@
-// function AdminDashboard() {
-//   return (
-//     <div style={{ textAlign: "center", marginTop: "100px" }}>
-//       <h2>Admin Dashboard</h2>
-//     </div>
-//   );
-// }
-
-// export default AdminDashboard;
-
-
-// function AdminDashboard() {
-//   return (
-//     <div style={{ textAlign: "center", marginTop: "100px" }}>
-//       <h2>Admin Dashboard</h2>
-//     </div>
-//   );
-// }
-
-// export default AdminDashboard;
-
-
-// function AdminDashboard() {
-//   return (
-//     <div style={{ textAlign: "center", marginTop: "100px" }}>
-//       <h2>Admin Dashboard</h2>
-//     </div>
-//   );
-// }
-
-// export default AdminDashboard;
-
-
 
 import React from "react";
 
